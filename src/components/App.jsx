6c476768aaed5791970fc5f6a9fe632dc5f124b9
OmarExpo <75645542+OmarExpo@users.noreply.github.com>
2022-08-13 16:10:53 +0200

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import CreateArea from "./CreateArea";
+
+function App() {
+  const [items, setItems] = useState([]);
+
+  function addItem(note) {
+    setItems((prevItem) => {
+      return [...prevItem, note];
+    });
+  }
+
+  function deleteNote(id) {
+    setItems((prevNote) => {
+      return prevNote.filter((item, index) => {
+        return index !== id;
+      });
+    });
+  }
+
+  return (
+    <div>
+      <Header />
+      <CreateArea onAdd={addItem} />
+      {items.map((noteItem, index) => {
+        return (
+          <Note
+            key={index}
+            id={index}
+            title={noteItem.title}
+            content={noteItem.content}
+            onDelete={deleteNote}
+          />
+        );
+      })}
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/CreateArea.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+
+function CreateArea(props) {
+  const [note, setNote] = useState({
+    title: "",
+    content: ""
+  });
+
+  function handleChange(e) {
+    const { name, value } = e.target;
+    //console.log(value);
+    setNote((prevNote) => {
+      return {
+        ...prevNote,
+        [name]: value
+      };
+    });
+  }
+
+  function handleClick(e) {
+    props.onAdd(note);
+    e.preventDefault();
+    setNote({ title: "", content: "" });
+  }
+
+  return (
+    <div>
+      <form>
+        <input
+          onChange={handleChange}
+          name="title"
+          placeholder="Title"
+          value={note.title}
+        />
+        <textarea
+          onChange={handleChange}
+          name="content"
+          placeholder="Take a note..."
+          rows="3"
+          value={note.content}
+        />
+        <button onClick={handleClick}>Add</button>
+      </form>
+    </div>
+  );
+}
+
+export default CreateArea;
